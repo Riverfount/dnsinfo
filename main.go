dba636c67478eb9c1fed82b3d28c8e327d8605cd
Vicente Marçal <vicente.marcal@gmail.com>
2026-06-27 21:48:32 -0300

    refactor: extract host normalization into normalizeHost function

--- main.go
+++ main.go
@@ -1,25 +1,39 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/url"
 	"os"
 	"strings"
 )
 
+func normalizeHost(urlRaw string) (string, error) {
+	if !strings.Contains(urlRaw, "://") {
+		urlRaw = "//" + urlRaw
+	}
+	urlParsed, err := url.Parse(urlRaw)
+	if err != nil {
+		return "", errors.New("error parse url")
+	}
+	hostName := urlParsed.Hostname()
+	if hostName == "" {
+		return "", errors.New("hostname is empty")
+	}
+
+	return hostName, nil
+
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Fprintln(os.Stderr, "Need to be informed the hostname!")
 		os.Exit(1)
 	}
-	urlRaw := os.Args[1]
-	if !strings.Contains(urlRaw, "//") {
-		urlRaw = "//" + urlRaw
-	}
-	urlParsed, err := url.Parse(urlRaw)
+	host, err := normalizeHost(os.Args[1])
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error parse URL")
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	fmt.Println(urlParsed.Hostname())
+	fmt.Println(host)
 }
